5162476019bc734d57e5712fe1ea3f988e4eb039
Kacper Rabczewski <krabczewski@gmail.com>
2020-05-15 01:02:35 +0200

    Add babel loader

--- webpack.config.js
+++ webpack.config.js
@@ -21,7 +21,7 @@ module.exports = {
             {
                 test: /\.scss$/,
                 use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
-            },
+            }
         ]
     },
     plugins: [
